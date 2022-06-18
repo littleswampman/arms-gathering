a70d25b0cbb243e4452c0a5adcb3cf9b2af1fef4
littleswampman <littleswampman@gmail.com>
2022-06-18 15:29:24 +0900

    fix layout.tsx

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,10 +1,25 @@
 import React, { FC } from "react";
+import { useSetRecoilState, SetterOrUpdater } from "recoil";
+
 import { Layout } from "../layout/Layout";
+import { displayedText } from "../types/displayedText";
+import { displayedTextState } from "../atoms/displayedTextState";
+import { TextDisplay } from "../components/TextDisplay";
 
 export const Battle: FC = () => {
+    const setDisplayedText: SetterOrUpdater<displayedText> =
+        useSetRecoilState(displayedTextState);
+    setDisplayedText({
+        texts: ["たたかい"],
+    });
     return (
         <Layout>
             <div>Battle</div>
+            <div className="relative h-full w-full">
+                <div className="absolute bottom-0 w-full">
+                    <TextDisplay />
+                </div>
+            </div>
         </Layout>
     );
 };
