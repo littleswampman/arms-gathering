c29a06a031003275a4c4ffc14ccc986f49575a93
littleswampman <littleswampman@gmail.com>
2022-06-18 15:28:05 +0900

    fix the text-display component to be able to display multiple lines

--- src/components/TextDisplay.tsx
+++ src/components/TextDisplay.tsx
@@ -5,31 +5,73 @@ import { displayedTextState } from "../atoms/displayedTextState";
 import styles from "./text-display.module.scss";
 
 export const TextDisplay: FC = () => {
-    let startSecond = 0;
-    const texts = useRecoilValue(displayedTextState).texts;
+    let textStartSecond = 0;
+    let lineStartSecond = 0;
+    const splitArray = (arr: string[], n: number) => {
+        // 配列arrをn個の要素ごとに分割する関数
+        const repeat = Math.ceil(arr.length / n);
+        return new Array(repeat)
+            .fill(0)
+            .map((_: 0, i: number) => arr.slice(i * n, (i + 1) * n));
+    };
+    // const sliceByNumber = (array, number) => {
+    //     const length = Math.ceil(array.length / number)
+    //     return new Array(length).fill().map((_, i) =>
+    //       array.slice(i * number, (i + 1) * number)
+    //     )
+    //   }
+    const lines = 3; // 一度に表示する最大行数
+    const texts = splitArray(useRecoilValue(displayedTextState).texts, lines); // lines行ごとに分割されている
+    console.log(texts);
+
     return (
         <div
             className={
                 styles["wrapper"] +
                 " " +
-                "rounded-md border-4 border-solid border-slate-100 p-4"
+                "relative rounded-md border-4 border-solid border-slate-100 p-4"
             }
         >
-            {texts.map((text) => {
-                let textLength = text.length;
-                let p_style_typewriter = {
-                    "--length": textLength,
-                    "animation-delay": startSecond + "s",
+            {texts.map((line, i, array) => {
+                let lineTextLength = line
+                    .map((text) => text.length)
+                    .reduce((sum, len) => sum + len, 0);
+                // 表示する行に含まれる文字数
+                let lineLength = line.length;
+                // 表示する行数
+                let div_style_typewriterWrapper = {
+                    "--line-text-length": lineTextLength,
+                    "--line-length": lineLength,
+                    "animation-delay": lineStartSecond + "s",
+                    "animation-fill-mode":
+                        array.length === i + 1 ? "forwards" : "none",
                 } as React.CSSProperties;
-                startSecond += textLength * 0.2 + 0.2;
+                textStartSecond = lineStartSecond;
 
+                lineStartSecond += (lineTextLength + lineLength) * 0.2 + 0.2;
                 return (
-                    <p
-                        style={p_style_typewriter}
-                        className={styles["type-writer"]}
+                    <div
+                        style={div_style_typewriterWrapper}
+                        className={styles["type-writer_wrapper"]}
                     >
-                        {text}
-                    </p>
+                        {line.map((text) => {
+                            let textLength = text.length;
+                            let p_style_typewriter = {
+                                "--text-length": textLength,
+                                "animation-delay": textStartSecond + "s",
+                            } as React.CSSProperties;
+                            textStartSecond += textLength * 0.2 + 0.2;
+
+                            return (
+                                <p
+                                    style={p_style_typewriter}
+                                    className={styles["type-writer"]}
+                                >
+                                    {text}
+                                </p>
+                            );
+                        })}
+                    </div>
                 );
             })}
         </div>
