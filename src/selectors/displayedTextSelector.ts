6e30f67487ade48951fe4a90bde0feadceb0e807
littleswampman <littleswampman@gmail.com>
2022-07-15 15:48:13 +0900

    lint displayTextSelector.ts

--- src/selectors/displayedTextSelector.ts
+++ src/selectors/displayedTextSelector.ts
@@ -1,13 +1,11 @@
 import { selector } from "recoil";
 import { displayedTextAtom } from "../atoms/displayedTextAtom";
-import { displayedText } from "../types/displayedText";
+import { DisplayedText } from "../types/DisplayedText";
 
-export const displayedTextSelector = selector<displayedText>({
+export const displayedTextSelector = selector<DisplayedText>({
     key: "displayedTextSelector",
     // getは{ get }を引数に取る関数
-    get: ({ get }) => {
-        return get(displayedTextAtom);
-    },
+    get: ({ get }) => get(displayedTextAtom),
 });
 
 export const processedDisplayedTextSelector = selector<string[][]>({
@@ -19,16 +17,19 @@ export const processedDisplayedTextSelector = selector<string[][]>({
         const splitArray = (textArray: string[], n: number) => {
             // 配列textArrayをn個の要素ごとに分割する関数
             const repeat = Math.ceil(textArray.length / n);
+
             return new Array(repeat)
                 .fill(0)
                 .map((_: 0, i: number) => textArray.slice(i * n, (i + 1) * n));
         };
         const limitedArray = (textArray: string[], limit: number) => {
             // 配列textArrayの要素における文字数を最大limitになるよう分割する関数
-            // TODO 高速化の余地あり
+            // TODO 色々と雑な実装なので直す
             while (textArray.some((el) => el.length > limit)) {
                 const temp = textArray;
+                // eslint-disable-next-line no-param-reassign
                 textArray = [];
+                // eslint-disable-next-line @typescript-eslint/no-loop-func
                 temp.map((text) =>
                     text.length <= limit
                         ? textArray.push(text)
@@ -38,12 +39,14 @@ export const processedDisplayedTextSelector = selector<string[][]>({
                           ),
                 );
             }
+
             return textArray;
         };
         const texts = splitArray(
             limitedArray(get(displayedTextAtom).texts, rows),
             lines,
         );
+
         return texts; // 最大rows文字のlines行ごとに分割されている
     },
 });
