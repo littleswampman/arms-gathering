796df5e662b5c31bc15b3d5cd806201fe923e9fe
littleswampman <littleswampman@gmail.com>
2022-07-16 12:10:53 +0900

    change file structure & GameProgress "start_arms-setting" -> "start_select-arms" "game_selecting-room" -> "game_select-room"

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -3,8 +3,8 @@ import { useSetRecoilState, SetterOrUpdater, useRecoilValue } from "recoil";
 
 import { Layout } from "../layout/Layout";
 
-import { NameInput } from "../components/NameInput";
-import { GameStart } from "../components/GameStart";
+import { NameInput } from "../components/start/NameInput";
+import { GameStart } from "../components/start/GameStart";
 
 import { TextDisplay } from "../components/TextDisplay";
 
