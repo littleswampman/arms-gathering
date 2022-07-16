339787b64dda2c68821a74b0d5c6c71bbb661c2b
littleswampman <littleswampman@gmail.com>
2022-07-17 01:02:34 +0900

    add createArm.ts

---
+++ src/services/createArm.ts
@@ -0,0 +1,25 @@
+import { Arm, whereToWear } from "../types/ArmType";
+import { BaseStatus, baseStatusArray } from "../types/BaseStatusType";
+
+export const createArm = (
+    luck: number,
+    rare: number,
+    specified?: typeof whereToWear[number],
+) => {
+    const wear =
+        specified ??
+        whereToWear[Math.floor(Math.random() * whereToWear.length)];
+    const status =
+        baseStatusArray[Math.floor(Math.random() * baseStatusArray.length)];
+    const effect = Math.ceil(rare * 10 + luck * Math.random() * 1);
+
+    return {
+        whereToWear: wear,
+        wearEffect: [
+            {
+                status,
+                effect,
+            },
+        ],
+    } as Arm;
+};
