bfdf5aa3777c109ee345bc108940fa2c19372850
littleswampman <littleswampman@gmail.com>
2022-07-17 11:05:43 +0900

    change main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Navigate, Route, Routes } from "react-router";
 import { RecoilRoot } from "recoil";
 
-import { Battle } from "./pages/Battle";
-import { GameOver } from "./pages/GameOver";
-import { Playing } from "./pages/Playing";
-import { Start } from "./pages/Start";
+import { ManageGameProgress } from "./components/ManageGameProgress";
 
 import "./index.css";
 
@@ -16,12 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <React.StrictMode>
             <RecoilRoot>
                 <Routes>
-                    <Route path="battle" element={<Battle />} />
-                    <Route path="game-over" element={<GameOver />} />
-                    <Route path="playing" element={<Playing />} />
-                    <Route path="start" element={<Start />} />
+                    <Route path="" element={<ManageGameProgress />} />
 
-                    <Route path="*" element={<Navigate to="start" />} />
+                    <Route path="*" element={<Navigate to="" />} />
                 </Routes>
             </RecoilRoot>
         </React.StrictMode>
