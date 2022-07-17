88a061c86e02049ee35ec74e6e003383b51c4150
littleswampman <littleswampman@gmail.com>
2022-07-17 11:04:51 +0900

    add ManageGameProgress

---
+++ src/components/ManageGameProgress.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useEffect } from "react";
+
+import { useRecoilValue, SetterOrUpdater, useSetRecoilState } from "recoil";
+
+import { Room } from "../pages/Room";
+import { GameOver } from "../pages/GameOver";
+import { Game } from "../pages/Game";
+import { Start } from "../pages/Start";
+
+import { GameProgress } from "../types/GameProgressType";
+import { gameProgressAtom } from "../atoms/gameProgressAtom";
+import { gameProgressSelector } from "../selectors/gameProgressSelector";
+
+export const ManageGameProgress: FC = () => {
+    const setGameProgress: SetterOrUpdater<GameProgress> =
+        useSetRecoilState(gameProgressAtom);
+
+    useEffect(() => {
+        setGameProgress("start_name-input");
+    }, [setGameProgress]);
+
+    const gameProgress = useRecoilValue(gameProgressSelector);
+    const switchElement = (gameProg: string) => {
+        switch (true) {
+            case /start_.*/.test(gameProg):
+                return <Start />;
+            case /game_.*/.test(gameProg):
+                return <Game />;
+            case /room.*/.test(gameProg):
+                return <Room />;
+            case /game-over_.*/.test(gameProg):
+                return <GameOver />;
+            case /game-clear.*/.test(gameProg):
+                return <p>error!! game-clear is not defined</p>;
+            default:
+                return <p>Error!</p>;
+        }
+    };
+
+    return <>{switchElement(gameProgress)}</>;
+};
