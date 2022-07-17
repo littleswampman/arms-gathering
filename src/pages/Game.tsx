2e49c634d936d686be324813c3508e1c1a466575
littleswampman <littleswampman@gmail.com>
2022-07-17 15:09:28 +0900

    fix Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,28 +1,37 @@
 import React, { FC } from "react";
-import { useSetRecoilState, SetterOrUpdater } from "recoil";
+import { useSetRecoilState, SetterOrUpdater, useRecoilValue } from "recoil";
 
 import { Layout } from "../layout/Layout";
-import { DisplayedText } from "../types/DisplayedTextType";
-import { displayedTextAtom } from "../atoms/displayedTextAtom";
+
+import { GameStart } from "../components/game/GameStart";
+
+import { GameProgress } from "../types/GameProgressType";
+import { gameProgressAtom } from "../atoms/gameProgressAtom";
+import { gameProgressSelector } from "../selectors/gameProgressSelector";
 
 export const Game: FC = () => {
-    const setDisplayedText: SetterOrUpdater<DisplayedText> =
-        useSetRecoilState(displayedTextAtom);
-    setDisplayedText({
-        texts: [
-            "てきがあらわれた！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！",
-            "ぶきをもて！！",
-            "たちむかうんだ！！",
-            "まだあるのか！！",
-            "おかわりもいいぞ！",
-            "もうちっとだけ続くんじゃ",
-            "俺たちの戦いはこれからだ！",
-        ],
-    });
+    const setGameProgress: SetterOrUpdater<GameProgress> =
+        useSetRecoilState(gameProgressAtom);
+    const gameProgress = useRecoilValue(gameProgressSelector);
+
+    const switchElement = () => {
+        switch (gameProgress) {
+            case "game_start":
+                return <GameStart />;
+            case "game_select-room":
+                return <p>a</p>;
+            case "game_room":
+                return <p>a</p>;
+            case "game_change-floor":
+                return <p>a</p>;
+            default:
+                return <p>Error!</p>;
+        }
+    };
 
     return (
         <Layout>
-            <div>Playing</div>
+            <div className="h-full w-full">{switchElement()}</div>
         </Layout>
     );
 };
